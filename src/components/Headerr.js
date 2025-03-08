2d719c0896b99d3a8753d84ca13554fc2406c907
Dhruvin <therockstar3007@gmail.com>
2025-03-08 19:45:33 +0530

    Header UI update

--- src/components/Headerr.js
+++ src/components/Headerr.js
@@ -10,7 +10,7 @@ export const Headerr = () => {
   const [hidden,setHidden]=useState(false);
 
   const [isAuth, setIsAuth] = useState(false);
-  const activeClass = "block py-2 px-3 text-blue-700 bg-blue-700 rounded-full md:text-blue-700 md:p-0 dark:text-blue-500 bg-slate-200";
+  const activeClass = "block py-2 px-3 text-blue-700 bg-blue-700 rounded-full md:text-blue-700 md:p-0 dark:text-blue-500 dark:bg-black bg-slate-200";
   const inactiveClass = "block text-base py-2 px-3 text-gray-900  bg-slate-200  md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-black dark:hover:text-white md:dark:hover:bg-transparent dark:border-black dark:bg-black";
   
   const navigate=useNavigate();
@@ -56,20 +56,20 @@ const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMo
 
   return (
     <header className="bg-transparent">
-      <nav className="bg-slate-200  dark:bg-black px-2 sm:px-4 py-0">
+      <nav className="bg-slate-200  dark:bg-black pl-2 sm:px-4 py-0">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
           <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={Logo} className="md:h-14 h-10" alt="Cinemate_by_Dhruvin" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MovieMate</span>
+            <img src={Logo} className="h-12" alt="Cinemate_by_Dhruvin" />
+            <span className="self-center md:text-2xl text-xl font-semibold whitespace-nowrap dark:text-white">MovieMate</span>
           </Link>
 
 
           <div id="modile-nav" className="flex md:order-2">
             
-          <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border-2 border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+          <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="flex items-center p-2 mr-2 text-lg font-medium text-gray-700 bg-white md:dark:bg-gray-700 dark:bg-black rounded-lg border-1 border-gray-700 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700  focus:ring-2 focus:ring-gray-300  focus:outline-none dark:text-gray-400  dark:hover:text-white dark:hover:bg-gray-700">
             { darkMode ? (<svg aria-hidden="true" data-toggle-icon="sun" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path></svg>) : (<svg aria-hidden="true" data-toggle-icon="moon" className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>) }
           </button>
-            <button onClick={()=>setHidden(!hidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none bg-white  rounded-lg text-sm p-2.5 me-1">
+            <button onClick={()=>setHidden(!hidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none   rounded-lg text-sm p-2.5 me-1">
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
@@ -91,7 +91,7 @@ const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMo
               </div>
             
             </div>
-            <button onClick={()=>setHidden(!hidden)}   data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
+            <button onClick={()=>setHidden(!hidden)}   data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
